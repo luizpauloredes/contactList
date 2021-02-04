5d9f75deb9a41e75c0df8edefbbe901e87ff6ddb
unknown <luizpaulo.redes@gmail.com>
2021-02-04 09:03:40 -0300

    delete contact function working

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -66,6 +66,22 @@ const getState = ({ getStore, setStore }) => {
 						})
 						.catch(e => console.error(e));
 				});
+			},
+
+			deleteContact(id) {
+				fetch(url + id, {
+					method: "delete"
+				}).then(() => {
+					fetch(url + "agenda/downtown_xii")
+						.then(response => response.json())
+						.then(result => {
+							console.log("result", result),
+								setStore({
+									contacts: result
+								});
+						})
+						.catch(e => console.error(e));
+				});
 			}
 		}
 	};
